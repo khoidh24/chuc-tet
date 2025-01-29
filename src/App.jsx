cff20f632d93595f58b24c4a2a8c78d6b73a6d93
Hoàng <zenorn24@gmail.com>
2025-01-29 11:53:11 +0700

    fix color of Volume icon

--- src/App.jsx
+++ src/App.jsx
@@ -142,7 +142,7 @@ const LuckyWheel = () => {
           className="absolute top-4 left-4 bg-[#CD1928] p-2 rounded-full shadow-md z-50 outline-none ring-0 focus:outline-none focus:ring-0"
         >
           {isAudioPlaying ? (
-            <Volume2 size={24} color="#FEF9C6]" />
+            <Volume2 size={24} color="#FEF9C6" />
           ) : (
             <VolumeX size={24} color="#FEF9C6" />
           )}
