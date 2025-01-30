f579ff9ae45d7f54749885ad719aeefa333d1714
Hoàng <zenorn24@gmail.com>
2025-01-31 00:01:29 +0700

    minify Js and CSS build

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    minify: "esbuild",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
 });
